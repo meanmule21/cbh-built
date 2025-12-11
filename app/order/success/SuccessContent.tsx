f083241675b4eb05c3c758d249768de2916877ab
Stephen Hoelscher <shoelscher6@gmail.com>
2025-12-10 21:45:00 -0600

    Update success page labels

--- app/order/success/SuccessContent.tsx
+++ app/order/success/SuccessContent.tsx
@@ -40,9 +40,9 @@ interface OrderDetails {
 }
 
 const frontLocationLabels: Record<string, string> = {
-  "front-center": "Center",
-  "front-left": "Over Left Eye",
-  "front-right": "Over Right Eye",
+  "front-center": "Front Center",
+  "front-left": "Front Left Panel",
+  "front-right": "Front Right Panel",
 };
 
 const extraLocationLabels: Record<string, string> = {
